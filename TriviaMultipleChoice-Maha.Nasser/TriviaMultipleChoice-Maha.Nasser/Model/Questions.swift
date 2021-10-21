2e95c42a47f1d37d92a27ef07750bf4950b382b1
Maha S <mahas@Mahas-MacBook-Air.local>
2021-10-21 15:33:28 +0300

    MultipleChoiceQuestions App.

---
+++ TriviaMultipleChoice-Maha.Nasser/TriviaMultipleChoice-Maha.Nasser/Model/Questions.swift
@@ -0,0 +1,27 @@
+//
+//  Questions.swift
+//  TriviaMultipleChoice-Maha.Nasser
+//
+//  Created by Maha S on 21/10/2021.
+//
+
+import Foundation
+
+struct question {
+    let question : String
+    let answer : [String]
+    let correctAnswer : String
+
+
+func getQuestion() -> String {
+    return self.question
+}
+
+func getAnswer() -> String {
+    return self.question
+}
+
+func getCorrectAnswer() -> String{
+    return self.question
+}
+}
